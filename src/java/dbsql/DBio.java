b4ea348edd667bde56dac4138ec46a9665e71baf
user <user@192.168.1.12>
2014-10-02 02:49:07 -0600

    comit

--- src/java/dbsql/DBio.java
+++ src/java/dbsql/DBio.java
@@ -15,142 +15,141 @@ import javax.swing.JOptionPane;
  */
 public class DBio {
     //CONSTANTES
+
     public final int SESSION_ANGEL = 1;
     public final int SESSION_ENTIDAD = 2;
     public final String ADMINISTRADOR = "Admin";
     public final String NORMAL = "Normal";
     OperacionesAUX a;
-    
     private Conexion conect = new Conexion();
     private Connection conexion = null;
-    private Statement declaracion=null; //Realizar las declraciones de SQL
-    private ResultSet resultado=null; //Obtner el resultado de dicha declaracion    
+    private Statement declaracion = null; //Realizar las declraciones de SQL
+    private ResultSet resultado = null; //Obtner el resultado de dicha declaracion    
     private String tipoUsuario = null;
     private String nombreEntidad = null;
-    private ArrayList <Solicitud> solicitudes = null;
+    private ArrayList<Solicitud> solicitudes = null;
     private Date fecha = null;
     private CallableStatement procNuevaSol;
     private int UserAngel = 0;
-    
     private String error = null;
-    
-    public DBio(){
-       conexion = conect.gestionarConexion();
-       if( conexion != null ){
-            try{
+
+    public DBio() {
+        conexion = conect.gestionarConexion();
+        if (conexion != null) {
+            try {
                 a = new OperacionesAUX(conexion);
                 declaracion = conexion.createStatement();
-            }catch(Exception ex){
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
-       error = conect.getError();
+        error = conect.getError();
     }
 
-    public void ingresarEntidad(){
-        try{
+    public void ingresarEntidad() {
+        try {
             declaracion = conect.getConexion().createStatement();
             declaracion.executeUpdate("insert into Entidad (CodigoE,Nombre) values (1,Ejemplo)");
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-    
-    public int comprobarLogin(String nombre, String pass){
-        try{
+
+    public int comprobarLogin(String nombre, String pass) {
+        try {
             //JOptionPane.showMessageDialog(null, "yeah");
-            resultado = declaracion.executeQuery("select Password, TipoUser, CodigoUA from UsuarioAngel where Alias='"+nombre+"'");
+            resultado = declaracion.executeQuery("select Password, TipoUser, CodigoUA from UsuarioAngel where Alias='" + nombre + "'");
             //JOptionPane.showMessageDialog(null, "yeah");
-            if( resultado.next() ){
-                String password = (String)resultado.getObject(1);
+            if (resultado.next()) {
+                String password = (String) resultado.getObject(1);
                 int tipo = Integer.parseInt(String.valueOf(resultado.getObject(2)));
                 UserAngel = Integer.parseInt(String.valueOf(resultado.getObject(3)));
                 tipoUsuario = TipoUsuario.getString(tipo);
-                if( password.equals(pass) ){
+                if (password.equals(pass)) {
                     //iniciar sesion para Usuario Angel
                     this.cerrar();
                     return SESSION_ANGEL;
                 }
-            }else{ //si no se encontro el usuario en UsuarioAngel se busca en UsuarioEnt
-                resultado = declaracion.executeQuery("select Password, TipoUser, CodigoE, CodigoUE from UsuarioEnt where Alias='"+nombre+"'");
-                if( resultado.next() ){
-                    String password = (String)resultado.getObject(1);
+            } else { //si no se encontro el usuario en UsuarioAngel se busca en UsuarioEnt
+                resultado = declaracion.executeQuery("select Password, TipoUser, CodigoE, CodigoUE from UsuarioEnt where Alias='" + nombre + "'");
+                if (resultado.next()) {
+                    String password = (String) resultado.getObject(1);
                     int tipo = Integer.parseInt(String.valueOf(resultado.getObject(2)));
                     tipoUsuario = TipoUsuario.getString(tipo);
                     int codigoE = Integer.parseInt(String.valueOf(resultado.getObject(3)));
                     UserAngel = Integer.parseInt(String.valueOf(resultado.getObject(4)));
-                    resultado = declaracion.executeQuery("select nombre from Entidad where CodigoE="+codigoE);
-                    if( resultado.next() ){
-                        nombreEntidad = (String)resultado.getObject(1);
+                    resultado = declaracion.executeQuery("select nombre from Entidad where CodigoE=" + codigoE);
+                    if (resultado.next()) {
+                        nombreEntidad = (String) resultado.getObject(1);
                     }
-                    if( password.equals(pass) ){
+                    if (password.equals(pass)) {
                         //iniciar sesion para Usuario Entidad
                         this.cerrar();
                         return SESSION_ENTIDAD;
                     }
                 }
             }
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
-            error = "Error procesando datos db: "+ex.getMessage();
+            error = "Error procesando datos db: " + ex.getMessage();
         }
         this.cerrar();
         return 0;
     }
-    
-    public int getCodigoUsuario(){
+
+    public int getCodigoUsuario() {
         return UserAngel;
     }
-    
-    private void procesarSolicitudesPendientes(){
+
+    private void procesarSolicitudesPendientes() {
         String consulta = "select * from Solicitud where Estado='Registrada'";
         solicitudes = new ArrayList();
         ResultSet resultado2 = null;
-        try{
+        try {
             resultado = declaracion.executeQuery(consulta);
             Statement declaracion2 = conexion.createStatement();
             int i = 0;
-            while( resultado.next() ){
-                String codigoEb = (String)resultado.getObject("CODIGOEB");
-                String codigoP = (String)resultado.getObject("CODIGOP");
+            while (resultado.next()) {
+                String codigoEb = (String) resultado.getObject("CODIGOEB");
+                String codigoP = (String) resultado.getObject("CODIGOP");
                 solicitudes.add(new Solicitud());
-                solicitudes.get(i).setCodigo( (String)resultado.getObject("CODIGOS") );
-                fecha = (Date)resultado.getObject("FECHA");
-                solicitudes.get(i).setFecha( fecha.toString() );
-                resultado2 = declaracion2.executeQuery("select Nombre, CodigoE from UsuarioEnt where CodigoUE='"+(String)resultado.getObject("CODIGOUE")+"'");
+                solicitudes.get(i).setCodigo((String) resultado.getObject("CODIGOS"));
+                fecha = (Date) resultado.getObject("FECHA");
+                solicitudes.get(i).setFecha(fecha.toString());
+                resultado2 = declaracion2.executeQuery("select Nombre, CodigoE from UsuarioEnt where CodigoUE='" + (String) resultado.getObject("CODIGOUE") + "'");
                 resultado2.next();
-                String codEntidad = (String)resultado2.getObject("CODIGOE");
-                solicitudes.get(i).setNombreUsuario( (String)resultado2.getObject("NOMBRE") );
-                resultado2 = declaracion2.executeQuery("select Nombre from Entidad where CodigoE='"+codEntidad+"'");
+                String codEntidad = (String) resultado2.getObject("CODIGOE");
+                solicitudes.get(i).setNombreUsuario((String) resultado2.getObject("NOMBRE"));
+                resultado2 = declaracion2.executeQuery("select Nombre from Entidad where CodigoE='" + codEntidad + "'");
                 resultado2.next();
-                solicitudes.get(i).setNombreEntidad( (String)resultado2.getObject("NOMBRE") );
-                resultado2 = declaracion2.executeQuery("select Nombre from Ebrio where CodigoEb='"+codigoEb+"'");
+                solicitudes.get(i).setNombreEntidad((String) resultado2.getObject("NOMBRE"));
+                resultado2 = declaracion2.executeQuery("select Nombre from Ebrio where CodigoEb='" + codigoEb + "'");
                 resultado2.next();
-                solicitudes.get(i).setNombreEbrio( (String)resultado2.getObject("NOMBRE") );
-                resultado2 = declaracion2.executeQuery("select Nombre from PilotoAngel where CodigoP='"+codigoP+"'");
+                solicitudes.get(i).setNombreEbrio((String) resultado2.getObject("NOMBRE"));
+                resultado2 = declaracion2.executeQuery("select Nombre from PilotoAngel where CodigoP='" + codigoP + "'");
                 resultado2.next();
-                solicitudes.get(i).setNombrePiloto( (String)resultado2.getObject("NOMBRE") );
+                solicitudes.get(i).setNombrePiloto((String) resultado2.getObject("NOMBRE"));
                 i++;
             }
             //JOptionPane.showMessageDialog(null, solicitudes.size());
-            if( resultado2 != null){
+            if (resultado2 != null) {
                 resultado2.close();
             }
-        }catch(Exception ex){
+        } catch (Exception ex) {
             error = ex.getMessage();
             JOptionPane.showMessageDialog(null, ex.getMessage());
             ex.printStackTrace();
-        }finally{
+        } finally {
             cerrar();
         }
     }
-    
-    public ArrayList<EntidadSolicitud> procesarTop5(){
+
+    public ArrayList<EntidadSolicitud> procesarTop5() {
         ArrayList<EntidadSolicitud> temp = new ArrayList();
         ArrayList<EntidadSolicitud> es = new ArrayList();
         String consulta1 = "select * from Entidad";
         String consulta2 = "select count(CodigoE) from Entidad"; //cuantas entidades hay
-        try{
+        try {
             ResultSet resultado2;
             Statement declaracion2 = conexion.createStatement();
             resultado = declaracion.executeQuery(consulta2);
@@ -158,49 +157,51 @@ public class DBio {
             String numEnts = String.valueOf(resultado.getObject("Count(CodigoE)"));
             int numEnt = Integer.parseInt(numEnts);
             resultado = declaracion.executeQuery(consulta1);
-            while( resultado.next() ){
-                String cod = (String)resultado.getObject("CodigoE");
-                resultado2 = declaracion2.executeQuery("select count(a.CodigoS) from Solicitud a JOIN (USUARIOENT b JOIN Entidad c ON b.CodigoE = "+cod+" and c.CODIGOE = "+cod+") ON b.CODIGOUE = a.CODIGOUE");
+            while (resultado.next()) {
+                String cod = (String) resultado.getObject("CodigoE");
+                resultado2 = declaracion2.executeQuery("select count(a.CodigoS) from Solicitud a JOIN (USUARIOENT b JOIN Entidad c ON b.CodigoE = " + cod + " and c.CODIGOE = " + cod + ") ON b.CODIGOUE = a.CODIGOUE");
                 resultado2.next();
-                es.add(new EntidadSolicitud((String)resultado.getObject("Nombre"),Integer.parseInt(String.valueOf(resultado2.getObject("Count(a.codigoS)")))));
+                es.add(new EntidadSolicitud((String) resultado.getObject("Nombre"), Integer.parseInt(String.valueOf(resultado2.getObject("Count(a.codigoS)")))));
             }
-            
+
             //ordenar el resultado
             EntidadSolicitud piv = null;
             int menor;
-            for(int i=0; i<es.size()-1; i++){
+            for (int i = 0; i < es.size() - 1; i++) {
                 menor = i;
-                for( int indice = i+1; indice<es.size(); indice++){
-                    if( es.get(indice).getNumSol() < es.get(menor).getNumSol() ){
+                for (int indice = i + 1; indice < es.size(); indice++) {
+                    if (es.get(indice).getNumSol() < es.get(menor).getNumSol()) {
                         menor = indice;
                     }
                 }
                 piv = es.get(i);
                 es.set(i, es.get(menor));
-                es.set(menor,piv);
+                es.set(menor, piv);
             }
             //organizar
-            for(int i=0; i<es.size(); i++){
-                temp.add(es.get(es.size()-(1+i)));
-                if( i == 4 ) break;
+            for (int i = 0; i < es.size(); i++) {
+                temp.add(es.get(es.size() - (1 + i)));
+                if (i == 4) {
+                    break;
+                }
             }
-        }catch(Exception ex){
+        } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, ex.getMessage());
             error = ex.getMessage();
             ex.printStackTrace();
         }
-        
+
         return temp;
     }
-    
+
     public boolean nuevaSolicitud(String eb_dpi, String eb_nombre, String eb_ape1, String eb_ape2,
-            String en_nombre, String en_ape, String en_tel, String cod_ue, String dep, String mun, String barrio){
-        
+            String en_nombre, String en_ape, String en_tel, String cod_ue, String dep, String mun, String barrio) {
+
         //String consulta_ebrio = "insert into Ebrio (DPI, Nombre, ApellidoP, ApellidoM) values ("+eb_dpi+",'"+eb_nombre+"','"+eb_ape1+"','"+eb_ape2+"')";
         //String cod_ebrio = null;
         //String consulta_encargado = "insert into EncargadoEbrio values ("+cod_ebrio+",'"+en_nombre+"','"+en_ape+"',"+en_tel+")";
         iniciarProcesoAlmacenado();
-        try{
+        try {
             //primero le asignamos los parametros
             procNuevaSol.setInt(1, Integer.parseInt(eb_dpi));
             procNuevaSol.setString(2, eb_nombre);
@@ -216,83 +217,74 @@ public class DBio {
             //ejecutamos el procedimiento almacenado
             procNuevaSol.execute();
             //luego leemos el dato de salida
-        }catch(Exception ex){
-            JOptionPane.showMessageDialog(null,ex.getMessage());
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
             ex.printStackTrace();
             return false;
         }
-        
+
         return true;
     }
-    
-    public ArrayList<Solicitud> getSolicitudesPendientes(){
+
+    public ArrayList<Solicitud> getSolicitudesPendientes() {
         procesarSolicitudesPendientes();
         return solicitudes;
     }
-    
-    public String getTipoUsuario(){
+
+    public String getTipoUsuario() {
         return tipoUsuario;
     }
-    
-    public String getError(){
+
+    public String getError() {
         return error;
     }
-    
-    public String getNombreEntidad(){
+
+    public String getNombreEntidad() {
         return nombreEntidad;
     }
-    
-    private void cerrar(){
-        try{
+
+    private void cerrar() {
+        try {
             declaracion.close();
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
-        try{
+        try {
             resultado.close();
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
-        try{
+        try {
             conect.cerrar();
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-    
-    private void iniciarProcesoAlmacenado(){
-        try{
+
+    private void iniciarProcesoAlmacenado() {
+        try {
             procNuevaSol = conexion.prepareCall("{call Nueva_Solicitud(?,?,?,?,?,?,?,?,?,?,?)");
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-    
+
     //Estos son mis metodos y manda a llamar los otros metodos de la clase OperacionesAUX
-    
-     //Estos son mis metodos y manda a llamar los otros metodos de la clase OperacionesAUX
-    
-     public void ingresarPiloto (int dpi, String Nombre, String ApellidoP, String ApellidoM,  String Genero, int Celular, int Domicilio){
-               a.PilotoAngel(dpi, Nombre, ApellidoP, ApellidoM,  Genero);
-               a.TelP( Celular, Domicilio);
-                    }      
+    //Estos son mis metodos y manda a llamar los otros metodos de la clase OperacionesAUX
+    public void ingresarPiloto(int dpi, String Nombre, String ApellidoP, String ApellidoM, String Genero, int Celular, int Domicilio) {
+        a.PilotoAngel(dpi, Nombre, ApellidoP, ApellidoM, Genero);
+        a.TelP(Celular, Domicilio);
+    }
 
-     public void UsuarioAngel (int DPI, String Nombre, String ApellidoP, String ApellidoM,String Genero, String Alias, String Password,int TipoUser, int Celular, int Domicilio){
-               a.UsuarioAng(DPI, Nombre, ApellidoP, ApellidoM, Genero,  Alias, Password, TipoUser);
-               a.TelUA(DPI, Celular, Domicilio);
-               
-                    }                        
-       
-       public void NuevaEntidad(String CodigoE, String Nombre,String Departamento,String Municipio,String Colonia,String Barrio,String Cacerío){
-           
-           a.Entidad( Nombre);
-           a.DireccionE(CodigoE, Departamento, Municipio, Colonia, Barrio, Cacerío);
-       }
-        
-    
-    
-    
-    
-    
+    public void UsuarioAngel(int DPI, String Nombre, String ApellidoP, String ApellidoM, String Genero, String Alias, String Password, int TipoUser, int Celular, int Domicilio) {
+        a.UsuarioAng(DPI, Nombre, ApellidoP, ApellidoM, Genero, Alias, Password, TipoUser);
+        a.TelUA(DPI, Celular, Domicilio);
+
+    }
+
+    public void NuevaEntidad(String CodigoE, String Nombre, String Departamento, String Municipio, String Colonia, String Barrio, String Cacerío) {
+
+        a.Entidad(Nombre);
+        a.DireccionE(CodigoE, Departamento, Municipio, Colonia, Barrio, Cacerío);
+    }
 }
-
--- src/java/dbsql/OperacionesAUX.java
+++ src/java/dbsql/OperacionesAUX.java
@@ -1,5 +1,5 @@
-
 package dbsql;
+
 import driver.Conexion;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -9,144 +9,130 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class OperacionesAUX {
-    private Connection conex= null;
+
+    private Connection conex = null;
     private Statement consulta = null;
-    private ResultSet rs = null; 
+    private ResultSet rs = null;
     private String query = null;
-    
-    
+
     //Conexion con = new Conexion();
-    
-    public OperacionesAUX(Connection con){
-    
+    public OperacionesAUX(Connection con) {
+
         conex = con;
         try {
             consulta = conex.createStatement();
         } catch (SQLException ex) {
             Logger.getLogger(DBio.class.getName()).log(Level.SEVERE, null, ex);
         }
-}
-    
-  
-    public void TelP ( int Celular, int Domicilio){
-                query = "Insert Into TelP values ("+Celular+","+Domicilio+")";
-               try{
-                  consulta.executeUpdate(query);
-                 
-          } catch(Exception e){
-                            e.printStackTrace();
-                }
-
-}
-    
-  public void PilotoAngel (int dpi, String Nombre, String ApellidoP, String ApellidoM, String Genero){            
-         
-              query = "Insert Into PilotoAngel values ("+dpi+",'"+Nombre+"','"+ApellidoP+"','"+ApellidoM+"','"+Genero+"')";
-              
-            try{
-                consulta.executeUpdate(query);
-                
-            } catch(Exception e){
-                 e.printStackTrace();
-            }
-        }  
-    
-    
-   public void Entidad ( String Nombre){            
-         
-              query = "Insert Into Entidad values ('"+Nombre+"')";
-              
-            try{
-                consulta.executeUpdate(query);
-                
-            } catch(Exception e){
-                 e.printStackTrace();
-            }
-        }  
-    
-   public void DireccionE (String CodigoE, String Departamento,String Municipio,String Colonia,String Barrio,String Cacerío){            
-      
-       
-       
-              query = "Insert Into DireccionE values ('"+CodigoE+"','"+Departamento+"','"+Municipio+"','"+Colonia+"','"+Barrio+"','"+Cacerío+"')";
-              
-            try{
-                consulta.executeUpdate(query);
-                
-            } catch(Exception e){
-                 e.printStackTrace();
-            }
-        }   
-    
-  
-    
-     public void insertarEbrio(int CodigoEb, int DPI,String Nombre, String ApellidoP, String ApellidoM){
-                
-        query = "insert into Ebrio (CodigoEb,DPI,Nombre,ApellidoP,ApellidoM) values ("+CodigoEb+","+DPI+",'"+Nombre+"','"+ApellidoP+"','"+ApellidoM+"')";
-              
-        try{
-                              
-            consulta.executeUpdate(query);                 
-            consulta.close(); 
-           
-        }catch(Exception ev){
-         ev.printStackTrace();
-        }
-           
     }
-public void TelUE (String CodigoUE, String Celular, String Domicilio){
-                query = "Insert Into TelUE values ('"+CodigoUE+"','"+Celular+"','"+Domicilio+"')";
-               try{
-                  consulta.executeUpdate(query);
-                            } catch(Exception e){
-                            e.printStackTrace();
-                }
 
-} 
-    
- 
+    public void TelP(int Celular, int Domicilio) {
+        query = "Insert Into TelP values (" + Celular + "," + Domicilio + ")";
+        try {
+            consulta.executeUpdate(query);
 
-public void UsuarioAng (int DPI, String Nombre, String ApellidoP, String ApellidoM, String Alias, String Password, String Genero,int TipoUser){
-           
-        String query = "Insert Into UsuarioAngel (DPI,Nombre, ApellidoP, ApellidoM,Alias,Password,Genero,TipoUser) values ("+DPI+",'"+Nombre+"','"+ApellidoP+"','"+ApellidoM+"','"+Alias+"','"+Password+"','"+Genero+"',"+TipoUser+")";
-       
-            try{
-                consulta.executeUpdate(query);
-                consulta.close();
-            } catch(Exception e){
-                 e.printStackTrace();
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-   
-   
-   
-    public void TelUA (int DPI, int Celular, int Domicilio){
-                String Cod = "Select CodigoUA from UsuarioAngel where DPI = "+DPI+" "; 
-                int CodigoUA = 0;
-                 try{
-                    consulta = conex.createStatement();
-                     rs = consulta.executeQuery(Cod);
-                  while(rs.next()){
-                    CodigoUA = rs.getInt("CodigoUA");
-                    
-                    }          
-                } catch (Exception ex) { 
-           ex.printStackTrace();
-        } 
-                        
-                
-                query = "Insert Into TelUA values ("+CodigoUA+","+Celular+","+Domicilio+")";
-                     try{
-                            consulta.executeUpdate(query);
-                            } catch(Exception e){
-                            e.printStackTrace();
-                }
 
-} 
-    
+    }
 
+    public void PilotoAngel(int dpi, String Nombre, String ApellidoP, String ApellidoM, String Genero) {
 
+        query = "Insert Into PilotoAngel values (" + dpi + ",'" + Nombre + "','" + ApellidoP + "','" + ApellidoM + "','" + Genero + "')";
 
+        try {
+            consulta.executeUpdate(query);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Entidad(String Nombre) {
+
+        query = "Insert Into Entidad values ('" + Nombre + "')";
+
+        try {
+            consulta.executeUpdate(query);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void DireccionE(String CodigoE, String Departamento, String Municipio, String Colonia, String Barrio, String Cacerío) {
 
 
+
+        query = "Insert Into DireccionE values ('" + CodigoE + "','" + Departamento + "','" + Municipio + "','" + Colonia + "','" + Barrio + "','" + Cacerío + "')";
+
+        try {
+            consulta.executeUpdate(query);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertarEbrio(int CodigoEb, int DPI, String Nombre, String ApellidoP, String ApellidoM) {
+
+        query = "insert into Ebrio (CodigoEb,DPI,Nombre,ApellidoP,ApellidoM) values (" + CodigoEb + "," + DPI + ",'" + Nombre + "','" + ApellidoP + "','" + ApellidoM + "')";
+
+        try {
+
+            consulta.executeUpdate(query);
+            consulta.close();
+
+        } catch (Exception ev) {
+            ev.printStackTrace();
+        }
+
+    }
+
+    public void TelUE(String CodigoUE, String Celular, String Domicilio) {
+        query = "Insert Into TelUE values ('" + CodigoUE + "','" + Celular + "','" + Domicilio + "')";
+        try {
+            consulta.executeUpdate(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void UsuarioAng(int DPI, String Nombre, String ApellidoP, String ApellidoM, String Alias, String Password, String Genero, int TipoUser) {
+
+        String query = "Insert Into UsuarioAngel (DPI,Nombre, ApellidoP, ApellidoM,Alias,Password,Genero,TipoUser) values (" + DPI + ",'" + Nombre + "','" + ApellidoP + "','" + ApellidoM + "','" + Alias + "','" + Password + "','" + Genero + "'," + TipoUser + ")";
+
+        try {
+            consulta.executeUpdate(query);
+            consulta.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void TelUA(int DPI, int Celular, int Domicilio) {
+        String Cod = "Select CodigoUA from UsuarioAngel where DPI = " + DPI + " ";
+        int CodigoUA = 0;
+        try {
+            consulta = conex.createStatement();
+            rs = consulta.executeQuery(Cod);
+            while (rs.next()) {
+                CodigoUA = rs.getInt("CodigoUA");
+
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+
+        query = "Insert Into TelUA values (" + CodigoUA + "," + Celular + "," + Domicilio + ")";
+        try {
+            consulta.executeUpdate(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
